38844e6cb767ae74edb3ceb866ea4ff6503059eb
Leonel <leonel.menezes.lima@concrete.com.br>
2019-01-17 17:08:58 -0200

    Initial Commit

---
+++ magic-the-gathering/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  magic-the-gathering
+//
+//  Created by leonel.menezes.lima on 17/01/19.
+//  Copyright © 2019 leonel.menezes.lima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

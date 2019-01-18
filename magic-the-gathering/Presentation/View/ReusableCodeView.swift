a5932cacfc6b553da0254f422695f4860d55864b
Adann Simões <adann.sergio.simoes@accenture.com>
2019-01-18 13:45:18 -0200

    add codeview and reusablecodeview

---
+++ magic-the-gathering/Presentation/View/ReusableCodeView.swift
@@ -0,0 +1,24 @@
+//
+//  ReusableCodeView.swift
+//  magic-the-gathering
+//
+//  Created by adann.sergio.simoes on 18/01/2019.
+//  Copyright © 2019 leonel.menezes.lima. All rights reserved.
+//
+
+import Foundation
+
+protocol ReusableCodeView: AnyObject, CodeView  {
+    var needsSetup: Bool {get set}
+}
+
+extension ReusableCodeView {
+    func setupView() {
+        if needsSetup {
+            buildViewHierarchy()
+            setupConstraints()
+            setupAdditionalConfigurations()
+            needsSetup.toggle()
+        }
+    }
+}

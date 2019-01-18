a8f70ad815133da5f2966787d782cdea91fa2b63
Isabel <isabel.isaura.l.lima@concrete.com.br>
2019-01-18 14:35:55 -0300

    Add test spec files

---
+++ magic-the-gatheringTests/Model/CardSpec.swift
@@ -0,0 +1,9 @@
+//
+//  CardSpec.swift
+//  magic-the-gathering
+//
+//  Created by isabel.isaura.l.lima on 18/01/2019.
+//  Copyright © 2019 leonel.menezes.lima. All rights reserved.
+//
+
+import Foundation

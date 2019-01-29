2f0289ae422877d1bcdc45ea97b550043cf4c5a1
Gabriel <gbr.reynoso@icloud.com>
2019-01-29 12:10:28 -0200

    Finished MagicAPIService

---
+++ magic-the-gathering/Extensions/String+LocalizedError.swift
@@ -0,0 +1,9 @@
+//
+//  String+LocalizedError.swift
+//  magic-the-gathering
+//
+//  Created by gabriel.n.reynoso on 29/01/19.
+//  Copyright © 2019 leonel.menezes.lima. All rights reserved.
+//
+
+import Foundation

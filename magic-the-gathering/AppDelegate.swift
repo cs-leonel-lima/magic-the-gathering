2f5e6476ea2f6727431db98cfb1177a1cc12ff79
Isabel <isabel.isaura.l.lima@concrete.com.br>
2019-01-30 17:41:58 -0300

    Custom TabBar view

--- magic-the-gathering/AppDelegate.swift
+++ magic-the-gathering/AppDelegate.swift
@@ -16,7 +16,51 @@ extension AppDelegate {
     private func setupWindow() {
         self.window = UIWindow()
         self.window?.makeKeyAndVisible()
-        let mainView = SetTableViewController(style: .grouped, magicService: LocalMagicService())
+        let mainView = TesteVC()//SetTableViewController(style: .grouped, magicService: LocalMagicService())
         self.window?.rootViewController = mainView
     }
+}
+
+class TesteVC: UIViewController {
+    
+    let screen = ScreenTest()
+    
+    override func loadView() {
+        self.view = screen
+    }
+}
+
+class ScreenTest: UIView {
+    
+    let tabBar = CustomTabBar()
+    
+    override init(frame: CGRect = .zero) {
+        super.init(frame: frame)
+        backgroundColor = .gray
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension ScreenTest: CodeView {
+    func buildViewHierarchy() {
+        addSubview(tabBar)
+    }
+    
+    func setupConstraints() {
+        tabBar.snp.makeConstraints { make in
+            make.height.equalTo(41)
+            make.bottom.equalTo(safeAreaLayoutGuide.snp.bottom)
+            make.width.equalToSuperview()
+            make.left.right.equalToSuperview()
+        }
+    }
+    
+    func setupAdditionalConfigurations() {
+        
+    }
+    
 }
--- magic-the-gathering/Presentation/Scenes/Shared Views/CustomTabBar.swift
+++ magic-the-gathering/Presentation/Scenes/Shared Views/CustomTabBar.swift
@@ -2,7 +2,78 @@ import UIKit
 
 class CustomTabBar: UIView {
     
-    let setsLabel: UI
+    let horizontalStack: UIStackView = {
+        let stack = UIStackView(frame: .zero)
+        stack.axis = .horizontal
+        stack.distribution = .fillEqually
+        stack.spacing = 10
+        return stack
+    }()
     
+    let sets: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setTitle("Sets", for: .normal)
+        button.titleLabel?.textColor = .white
+        return button
+    }()
+    
+    let favs: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setTitle("Favorites", for: .normal)
+        button.titleLabel?.textColor = .white
+        return button
+    }()
+    
+    let middleSeparator: UIView = {
+        let view = UIView(frame: .zero)
+        view.frame.size = CGSize(width: 1.0, height: 28)
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    let topSeparator: UIView = {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension CustomTabBar: CodeView {
+    func buildViewHierarchy() {
+        horizontalStack.addArrangedSubview(sets)
+        horizontalStack.addArrangedSubview(middleSeparator)
+        addSubview(middleSeparator)
+        addSubview(horizontalStack)
+        addSubview(topSeparator)
+    }
+    
+    func setupConstraints() {
+        horizontalStack.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        topSeparator.snp.makeConstraints { make in
+            make.top.equalTo(horizontalStack.snp.top)
+            make.width.equalToSuperview().multipliedBy(0.85)
+            make.height.equalTo(1.0)
+            make.centerX.equalToSuperview()
+        }
+        
+//        middleSeparator.snp
+    }
+    
+    func setupAdditionalConfigurations() {
+        self.backgroundColor = .clear
+    }
     
 }

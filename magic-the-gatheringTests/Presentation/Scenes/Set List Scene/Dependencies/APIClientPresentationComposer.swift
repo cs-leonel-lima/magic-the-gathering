cd3ae3e5d41d93517177aa2f66eb74626f34a7e4
Gabriel <gbr.reynoso@icloud.com>
2019-01-30 16:55:13 -0200

    Some tests

---
+++ magic-the-gatheringTests/Presentation/Scenes/Set List Scene/Dependencies/APIClientPresentationComposer.swift
@@ -0,0 +1,9 @@
+//
+//  APIClientPresentationComposer.swift
+//  magic-the-gatheringTests
+//
+//  Created by gabriel.n.reynoso on 30/01/19.
+//  Copyright © 2019 leonel.menezes.lima. All rights reserved.
+//
+
+import Foundation
---
+++ magic-the-gatheringTests/Presentation/Scenes/Set List Scene/Dependencies/PresentationComposerSpec.swift
@@ -0,0 +1,9 @@
+//
+//  PresentationComposerSpec.swift
+//  magic-the-gatheringTests
+//
+//  Created by gabriel.n.reynoso on 30/01/19.
+//  Copyright © 2019 leonel.menezes.lima. All rights reserved.
+//
+
+import Foundation

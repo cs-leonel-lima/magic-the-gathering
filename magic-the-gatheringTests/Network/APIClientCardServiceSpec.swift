692088a72a4a744a4880584ea1ebeba0d0c8981a
Adann Simões <adann.sergio.simoes@accenture.com>
2019-01-30 17:15:24 -0200

    api client card test

---
+++ magic-the-gatheringTests/Network/APIClientCardServiceSpec.swift
@@ -0,0 +1,9 @@
+//
+//  APIClientCardServiceSpec.swift
+//  magic-the-gatheringTests
+//
+//  Created by adann.sergio.simoes on 30/01/2019.
+//  Copyright © 2019 leonel.menezes.lima. All rights reserved.
+//
+
+import Foundation

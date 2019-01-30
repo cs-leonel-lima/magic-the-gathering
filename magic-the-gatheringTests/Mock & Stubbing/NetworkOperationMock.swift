01a73f992d44255b8ed22ca9471b56ae4278f521
Adann Simões <adann.sergio.simoes@accenture.com>
2019-01-30 16:51:25 -0200

    API client set service test

--- magic-the-gatheringTests/Mock & Stubbing/NetworkOperationMock.swift
+++ magic-the-gatheringTests/Mock & Stubbing/NetworkOperationMock.swift
@@ -1,11 +1,3 @@
-//
-//  NetworkOperationMock.swift
-//  magic-the-gatheringTests
-//
-//  Created by gabriel.n.reynoso on 30/01/19.
-//  Copyright © 2019 leonel.menezes.lima. All rights reserved.
-//
-
 import Foundation
 @testable import magic_the_gathering
 

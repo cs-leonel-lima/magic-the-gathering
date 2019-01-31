99d94d8356062460504d650bf13033d0660e3f90
Adann Simões <adann.sergio.simoes@accenture.com>
2019-01-31 10:41:24 -0200

    add setheader snapshot

---
+++ magic-the-gatheringTests/Presentation/Scenes/Set List Scene/Snapshots/SetHeaderSectionTableViewQS.swift
@@ -0,0 +1,9 @@
+//
+//  SetHeaderSectionTableViewQS.swift
+//  magic-the-gatheringTests
+//
+//  Created by adann.sergio.simoes on 31/01/2019.
+//  Copyright © 2019 leonel.menezes.lima. All rights reserved.
+//
+
+import Foundation

75aaccfd1678bbc3c54d5bcc3416c2d6bd2b71b6
Isabel <isabel.isaura.l.lima@concrete.com.br>
2019-01-31 08:41:51 -0300

    Adding Snapshot tests

---
+++ magic-the-gatheringTests/Presentation/Scenes/Shared Views/CustomTabBarSnapshot.swift
@@ -0,0 +1,18 @@
+import Quick
+import Nimble
+import Nimble_Snapshots
+@testable import magic_the_gathering
+
+class CustomTabBarSnapshot: QuickSpec {
+    override func spec() {
+        var tabBar: CustomTabBar!
+        
+        beforeEach {
+            tabBar = CustomTabBar(frame: CGRect(x: 0, y: 0, width: 375, height: 55))
+        }
+        
+        it("has the expected look and feel") {
+            expect(tabBar) == snapshot("TabBar")
+        }
+    }
+}

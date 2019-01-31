092c6c881bd13b24c41596b4d013d64275209cf3
Adann Simões <adann.sergio.simoes@accenture.com>
2019-01-31 10:41:53 -0200

    fix tests

--- magic-the-gatheringTests/Presentation/Scenes/Set List Scene/Snapshots/SetHeaderSectionTableViewQS.swift
+++ magic-the-gatheringTests/Presentation/Scenes/Set List Scene/Snapshots/SetHeaderSectionTableViewQS.swift
@@ -1,9 +1,18 @@
-//
-//  SetHeaderSectionTableViewQS.swift
-//  magic-the-gatheringTests
-//
-//  Created by adann.sergio.simoes on 31/01/2019.
-//  Copyright © 2019 leonel.menezes.lima. All rights reserved.
-//
+import UIKit
+import Quick
+import Nimble
+import Nimble_Snapshots
 
-import Foundation
+@testable import magic_the_gathering
+
+class SetHeaderSectionTableViewQS: QuickSpec {
+    override func spec() {
+        describe("The 'UI'") {
+            it("should have the expected look and feel") {
+                let frame = CGRect(origin: .zero, size: CGSize(width: 500, height: 300))
+                let view = SetHeaderSectionTableView(frame: frame, titleForHeader: "Set name - It can be a huge naaaame")
+                expect(view) == snapshot("SetHeaderSectionTableViewQS")
+            }
+        }
+    }
+}
